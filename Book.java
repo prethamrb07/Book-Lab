e609b305bccce25870f4941fe4c42bbc5f86002d
prethamrb07 <prethamrb07@gmail.com>
2025-10-28 10:29:48 -0700

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,14 @@
+public class Book {
+   public String pigLatin(String word){
+
+   }
+   public int endPunctuation(String word){
+
+   }
+   public String translateWord(String word){
+
+   }
+   public String translateSentence(String word){
+
+   }
+}
