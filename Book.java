fdf6968ed15485890b8211337762d5121b9ef760
prethamrb07 <prethamrb07@gmail.com>
2025-11-03 10:29:38 -0800

    Add files via upload

--- Book.java
+++ Book.java
@@ -1,14 +1,39 @@
 public class Book {
    public String pigLatin(String word){
-
+      String vowel = "aeiou";
+      String numbers = "1234567890";
+      if(word.length()==0){
+         return word;
+      }
+      else if (vowel.indexOf(word.substring(0,1))>=0){
+         return word + "yay";
+      }else if(numbers.indexOf(word.substring(0,1))>=0){
+         return word + "ay";
+      }else if (word.length()==1){
+         return word+"ay";
+      }
+      for (int i = 0; i < word.length(); i++){
+         if (vowel.indexOf(word.substring(i, i+1))>=0){
+            String left = word.substring ( 0,i);
+            String right = word.substring (i+1,word.length());
+            return right+left+"ay";
+         }
+      }
    }
    public int endPunctuation(String word){
 
+
+      return -1;
    }
    public String translateWord(String word){
+      String convertedWord="";
 
+      return convertedWord;
    }
    public String translateSentence(String word){
+      String retSentence = "";
 
+
+      return retSentence;
    }
 }
