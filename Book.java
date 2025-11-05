a527387551cd088e2de381886ffeafff42d9632a
prethamrb07 <prethamrb07@gmail.com>
2025-11-04 17:38:35 -0800

    Add files via upload

--- Book.java
+++ Book.java
@@ -3,8 +3,20 @@ public class Book {
       String newWord = "";
       String vowel = "aeiouy";
       String numbers = "1234567890";
-      if(word.length()==0){
+      String punctuation = "!?.:;";
+      boolean a=false;
+       if(word.length()==0){
          return word;
+      }
+      for (int i = 0; i < word.length(); i++){
+         if (word.substring(i,i+1)=="-"){
+            a=true;
+         }
+      }
+      
+      
+      if(a){
+         return word+"yay";
       }
       else if (vowel.indexOf(word.substring(0,1))>=0){
          return word + "yay";
@@ -12,15 +24,31 @@ public class Book {
          return word + "ay";
       }else if (word.length()==1){
          return word+"ay";
-      }
+      }else{
+      
+
       for (int i = 0; i < word.length(); i++){
          if (vowel.indexOf(word.substring(i, i+1))>=0){
             String left = word.substring ( 0,i);
             String right = word.substring (i,word.length());
-            return right+left+"ay";
             
+           
+
+            char firstLetter = word.charAt(0);
+
+            if (Character.isUpperCase(firstLetter)){
+               String first = right.substring(0,1).toUpperCase();
+               String remain = right.substring(1,right.length());
+               String left2 = left.toLowerCase();
+               return first+remain+left2+"ay";
+
+            }else{
+               return right+left+"ay";
+            }
          }
       }
+   }
+      
       return word;
    }
    public int endPunctuation(String word){
