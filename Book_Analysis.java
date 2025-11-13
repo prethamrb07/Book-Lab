d4eb531294fdc1b2fff48e6de774e2322a68f141
prethamrb07 <prethamrb07@gmail.com>
2025-11-13 10:20:43 -0800

    Update Book_Analysis.java

--- Book_Analysis.java
+++ Book_Analysis.java
@@ -1,7 +1,10 @@
 class app {
    public static void main(String[] args){
       Book aBook = new Book();
-      System.out.println(aBook.pigLatin("hello"));
+      System.out.println(aBook.pigLatin("hello!"));
+      System.out.println(aBook.pigLatin("What?!?"));
+      System.out.println(aBook.pigLatin("Allons-y"));
+      System.out.println(aBook.translateSentence("I can now speak in Pig-Latin? WOW!!! I can translate words and sentences for the students at Foothill High School."));
     
    }
 }
