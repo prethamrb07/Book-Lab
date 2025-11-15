23fe39580a96efdff769f838d1268b5fbc176a76
prethamrb07 <prethamrb07@gmail.com>
2025-11-14 22:38:45 -0800

    Update book URL and add output messages

--- Book_Analysis.java
+++ Book_Analysis.java
@@ -1,6 +1,9 @@
 class app {
    public static void main(String[] args){
-      Book aBook = new Book("https://www.gutenberg.org/cache/epub/6130/pg6130.txt");
-      
+      //Book aBook = new Book("https://www.gutenberg.org/cache/epub/6130/pg6130.txt");
+      Book aBook = new Book("https://www.gutenberg.org/cache/epub/77236/pg77236.txt");
+      System.out.println("Total words: " + aBook.getCount());
+      aBook.saveToFile("translated_book.txt");
+      System.out.println("Book saved to translated_book.txt");
    }
 }
